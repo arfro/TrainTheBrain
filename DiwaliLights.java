133268d39dc9fc39f8d92591e448836cc54cdfcb
Annette <aerfromenes@gmail.com>
2017-08-30 13:07:47 +0100

    adding top comment to describe the problem

--- DiwaliLights.java
+++ DiwaliLights.java
@@ -1,3 +1,20 @@
+/*
+On the eve of Diwali, Hari is decorating his house with a serial light bulb set. The serial light bulb set has N bulbs placed sequentially on a string which is programmed to change patterns every second. If at least one bulb in the set is on at any given instant of time, how many different patterns of light can the serial light bulb set produce?
+
+Note: Lighting two bulbs *-* is different from **-
+
+Input Format 
+The first line contains the number of test cases T, T lines follow. 
+Each line contains an integer N, the number of bulbs in the serial light bulb set.
+
+Output Format 
+Print the total number of patterns modulo 105
+
+Constraints 
+1 <= T <= 1000 
+0< N < 100000
+*/
+
 import java.io.*;
 import java.util.*;
 
